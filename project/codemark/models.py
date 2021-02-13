a3ca18636931136ad6f9d18963f204fd38cdd154
Luke Zearfoss <LWZearfoss@gmail.com>
2021-02-13 12:34:51 -0800

    Updates User model and implements email password resets.

--- project/codemark/models.py
+++ project/codemark/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True, blank=False)
--- project/codemark/urls.py
+++ project/codemark/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path("password_reset", auth_views.PasswordResetView.as_view(template_name='password_reset/request.html', subject_template_name='password_reset/subject.txt', html_email_template_name='password_reset/email.html'), name="password_reset"),
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name='password_reset/done.html'), name="password_reset_done"),
+    path("password_reset_confirm/<uidb64>/<token>", auth_views.PasswordResetConfirmView.as_view(template_name='password_reset/confirm.html'), name="password_reset_confirm"),
+    path("password_reset_complete", auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/complete.html'), name="password_reset_complete"),
     path('', views.index_view, name='index'),
 ]
