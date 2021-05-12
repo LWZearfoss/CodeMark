69534b96fe9aa4e5cb84c0d9a0c06860f90e899d
Luke Zearfoss <lwzerafoss@gmail.com>
2021-05-12 07:20:59 +0000

    Adds Caddy configuration.

--- project/project/settings.py
+++ project/project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config['DEBUG']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['codemark.luke.zone', 'www.codemark.luke.zone']
 
 # Application definition
 
