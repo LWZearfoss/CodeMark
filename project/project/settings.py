ea496d896dfebf4cd486f877338499fe9b2dfc41
Luke Zearfoss <lwzerafoss@gmail.com>
2021-05-13 00:19:22 +0000

    Use Google Cloud Buckets for static files.

--- project/project/settings.py
+++ project/project/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 
 # Custom User Model
 
